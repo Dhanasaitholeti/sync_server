e96c4a41cdb6fb544105c433da1f5db5cb5bf1d6
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-09-10 11:30:47 +0530

    fixed some issues regarding socket events and removed unwanted helper functions

--- src/socket/events/chat.ts
+++ src/socket/events/chat.ts
@@ -3,12 +3,6 @@ import { sendDatatoConncetion } from "../../helpers/socket";
 import { Server, Socket } from "socket.io";
 
 const chat = async (io: Server, socket: Socket) => {
-  socket.on("GetChatMessages", async (Data) => {
-    console.log("Inside chats events");
-    const ChatMessages = await getChatMessagesWithChatId(Data.chatId);
-    socket.emit("chatmessages", ChatMessages);
-  });
-
   socket.on("sendMessage", async (Data) => {
     await sendDatatoConncetion(io, Data);
     console.log(Data);
--- src/socket/events/initData.ts
+++ src/socket/events/initData.ts
@@ -1,9 +1,9 @@
-import { getUserChatsWithId } from "../../helpers";
+import { getUserChatsWithId, getUserDataWithEmail } from "../../helpers";
 import { Server, Socket } from "socket.io";
 
 const initData = async (io: Server, socket: Socket) => {
   const chatsData = await getUserChatsWithId(socket.userData.id.toString());
-  io.to(chatsData.connectionId).emit("chatsData", chatsData.chats);
+  io.to(chatsData.connectionId).emit("chatsData", chatsData);
 };
 
 export default initData;
